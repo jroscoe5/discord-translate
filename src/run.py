288f1fc43646afe884c817185daf90b4e0d6bdcb
jroscoe5 <36338176+jroscoe5@users.noreply.github.com>
2019-06-05 20:10:48 -0700

    Update run.py

--- src/run.py
+++ src/run.py
@@ -4,6 +4,6 @@ from os import sys
 from translatorbot import TranslatorBot
 import logger
 
-token = open('token\\token.dtoken').readline()
-bot = TranslatorBot(logger.Logger(sys.stdout, logger.Verbosity.high))
+token = open('token/token.dtoken').readline()
+bot = TranslatorBot(logger.Logger(sys.stdout, logger.Verbosity.none))
 bot.run(token)
