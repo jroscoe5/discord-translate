fb89c7527feb21691e09c155936600c17ad49791
jroscoe5 <36338176+jroscoe5@users.noreply.github.com>
2019-06-06 17:23:22 -0700

    Update run.py

--- src/run.py
+++ src/run.py
@@ -4,6 +4,6 @@ from os import sys, environ
 from translatorbot import TranslatorBot
 import logger
 
-token = open('token\\token.dtoken').readline()
-bot = TranslatorBot(logger.Logger(sys.stdout, logger.Verbosity.high))
+token = environ['token']
+bot = TranslatorBot(logger.Logger(sys.stdout, logger.Verbosity.none))
 bot.run(token)
